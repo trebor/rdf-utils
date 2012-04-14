c014849819a832c7add70c60668a98082f91fa6a
Robert Harris <github.com@trebor.org>
2012-04-14 09:49:19 -0700

    added file loading

--- src/main/java/org/trebor/util/rdf/RdfUtil.java
+++ src/main/java/org/trebor/util/rdf/RdfUtil.java
@@ -7,10 +7,13 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -31,8 +34,10 @@ import org.openrdf.query.QueryLanguage;
 import org.openrdf.query.TupleQuery;
 import org.openrdf.query.TupleQueryResult;
 import org.openrdf.repository.Repository;
+import org.openrdf.repository.RepositoryConnection;
 import org.openrdf.repository.RepositoryException;
 import org.openrdf.repository.RepositoryResult;
+import org.openrdf.repository.http.HTTPRepository;
 import org.openrdf.rio.RDFFormat;
 import org.openrdf.rio.RDFHandlerException;
 import org.openrdf.rio.RDFParseException;
@@ -328,6 +333,39 @@ public class RdfUtil
     }
   }
 
+  public static File[] loadAll(RepositoryConnection connection, File directory, URI context, final RDFFormat format) throws RDFParseException, RepositoryException, IOException, MalformedQueryException, QueryEvaluationException
+  {
+    File[] files = findFiles(directory, format);
+    for (File file: files)
+      connection.add(file, null, format, context);
+    
+    return files;
+  }
+
+  public static File[] findFiles(File directory, final RDFFormat format)
+  {
+    return directory.listFiles(new FilenameFilter()
+    {
+      List<String> extentions = format.getFileExtensions();
+      
+      public boolean accept(File dir, String name)
+      {
+        for (String extention: extentions)
+          if (name.endsWith(extention))
+            return true;
+        return false;
+      }
+    });
+  }
+
+  public static RepositoryConnection establishRepositoryConnection(String host, String name) throws RepositoryException
+  {
+    String url = String.format("http://%s/openrdf-sesame/repositories/%s", host, name);
+    Repository repository = new HTTPRepository(url);
+    repository.initialize();
+    return repository.getConnection();
+  }
+
   public static int showResult(GraphQueryResult result, Repository repository)
     throws QueryEvaluationException
   {
@@ -429,5 +467,39 @@ public class RdfUtil
     BindingSet result = countResult.next();
     count = ((Literal)result.getValue("count")).longValue();
     return count;
+  }
+  
+  public static Date getDate(RepositoryConnection connection, String query)
+  {
+    try
+    {
+      TupleQueryResult result =
+        connection.prepareTupleQuery(QueryLanguage.SPARQL, query).evaluate();
+      Value date = result.next().getValue("date");
+      return ((Literal)date).calendarValue().toGregorianCalendar().getTime();
+    }
+    catch (Exception e)
+    {
+      log.error("query: " + query, e);
+    }
+
+    return null;
+  }
+  
+  public static Integer getInt(RepositoryConnection connection, String query)
+  {
+    try
+    {
+      TupleQueryResult result =
+        connection.prepareTupleQuery(QueryLanguage.SPARQL, query).evaluate();
+      Value val = result.next().getValue("int");
+      return ((Literal)val).integerValue().intValue();
+    }
+    catch (Exception e)
+    {
+      log.error("query: " + query, e);
+    }
+
+    return null;
   }
 }
